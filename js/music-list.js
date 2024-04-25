de19c58555150f4a5a10a7e2f7ffad8b71f2f623
Shirishachelikani <134859663+Shirishachelikani@users.noreply.github.com>
2024-04-25 15:44:21 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,20 @@
+let allMusic = [
+    {
+      name: "Take Me Away",
+      artist: "New Medicine",
+      img: "music-1",
+      src: "music-1" 
+    },
+    {
+      name: "Don't-Let-Break-Your-Heart",
+      artist: " Louis Tomlinson",
+      img: "music-2",
+      src: "music-2"
+    },
+    {
+      name: "Heartattack",
+      artist: " Enrique Iglesias",
+      img: "music-4",
+      src: "music-4"
+    },
+  ];
